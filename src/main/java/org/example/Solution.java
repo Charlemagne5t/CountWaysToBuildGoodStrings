c9188b962b6d975209209ef097919be149b2f516
alek <wait82985@gmail.com>
2023-05-13 15:45:48 +0600

    working  solution.  all tests passed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -11,5 +11,33 @@ package org.example;
 public class Solution {
     public int countGoodStrings(int low, int high, int zero, int one) {
         int mod = (int) 1e9 + 7;
+        long[] dp = new long[high + 1];
+        dp[0] = 0;
+        if (zero == 1) {
+            dp[1] = 1;
+        }
+        if (one == 1) {
+            dp[1] = dp[1] + 1;
+        }
+
+        for (int i = 2; i <= high; i++) {
+
+            if (zero == i) {
+                dp[i] = dp[i] + 1;
+            }
+
+            if (one == i) {
+                dp[i] = dp[i] + 1;
+            }
+            if (i - zero > 0)
+                dp[i] = (dp[i] + dp[i - zero]) % mod;
+            if (i - one > 0)
+                dp[i] = (dp[i] + dp[i - one] % mod);
+        }
+        long sum = 0;
+        for (int i = low; i <= high; i++) {
+            sum += dp[i];
+        }
+        return (int) (sum % mod);
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -7,4 +7,19 @@ public class SolutionTest {
     public void countGoodStringsTest1(){
         Assert.assertEquals(8, new Solution().countGoodStrings(3, 3, 1, 1));
     }
+
+    @Test
+    public void countGoodStringsTest2(){
+        Assert.assertEquals(5, new Solution().countGoodStrings(2, 3, 1, 2));
+    }
+    @Test
+    public void countGoodStringsTest3(){
+        Assert.assertEquals(764262396, new Solution().countGoodStrings(200, 200, 10, 1));
+    }
+
+    @Test
+    public void countGoodStringsTest4(){
+        Assert.assertEquals(873327137, new Solution().countGoodStrings(500, 500, 5, 2));
+    }
 }
+
