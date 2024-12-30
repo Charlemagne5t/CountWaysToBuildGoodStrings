7c22223f169b640fa1a763e81b80368d23d47522
alek <wait82985@gmail.com>
2024-12-30 15:24:48 +0500

    problemm solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,43 +1,23 @@
 package org.example;
-//Given the integers zero, one, low, and high,
-// we can construct a string by starting with an empty string, and then at each step perform either of the following:
-//
-//    Append the character '0' zero times.
-//    Append the character '1' one times.
-//
-//This can be performed any number of times.
-//
-//A good string is a string constructed by the above process having a length between low and high (inclusive).
-public class Solution {
+class Solution {
     public int countGoodStrings(int low, int high, int zero, int one) {
-        int mod = 1000_000_007;
+        int mod = (int)1e9 + 7;
         int[] dp = new int[high + 1];
-        dp[0] = 0;
-        if (zero == 1) {
-            dp[1] = 1;
-        }
-        if (one == 1) {
-            dp[1] = dp[1] + 1;
-        }
-
-        for (int i = 2; i <= high; i++) {
-
-            if (zero == i) {
-                dp[i] = dp[i] + 1;
+        dp[0] = 1;
+        for(int i = 1; i < dp.length; i++) {
+            if(i - zero >= 0) {
+                dp[i] = dp[i - zero] % mod;
             }
-
-            if (one == i) {
-                dp[i] = dp[i] + 1;
+            if(i  - one >= 0) {
+                dp[i] = dp[i] % mod + dp[i - one] % mod;
             }
-            if (i - zero > 0)
-                dp[i] = (dp[i] + dp[i - zero]) % mod;
-            if (i - one > 0)
-                dp[i] = (dp[i] + dp[i - one]) % mod;
         }
-        int sum = 0;
-        for (int i = low; i <= high; i++) {
-            sum = (sum + dp[i]) % mod;
+        int res = 0;
+        for(int i = low; i <= high; i++) {
+            res = (res % mod + dp[i]% mod) % mod;
         }
-        return sum % mod;
+
+
+        return res;
     }
 }
